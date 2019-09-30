d66c31bac524a00713cffd3bdff59cea47845aca
NguyenTruongLinh <nhokkuteo1996@gmail.com>
2019-09-30 22:40:00 +0700

    Update send mail

--- js/main.js
+++ js/main.js
@@ -11,26 +11,11 @@ $(document).ready(function () {
         $('.popup').fadeOut(350);
         $('.popup .popup-wrapper').removeClass('animationOpen').addClass('animationClose');
         $('body').css('overflow', 'auto');
+        $('.send-infomation').removeAttr('disabled').html('Gửi thông tin');
+        $('.form-register')[0].reset();
     });
 
     // Send Email
-    $('.send-infomation').click(function (e) {
-        e.preventDefault();
-
-        $("#form-main .check").map(function(){
-            if( !$(this).val() ) {
-                $(this).addClass('input-error');
-            } else if ($(this).val()) {
-                var email = $('input[type="email"]').val();
-                if (!validateEmail(email)) {
-                    $('input[type="email"]').addClass('input-error');
-                } else {
-                    $(this).removeClass('input-error');
-                    sendmail();
-                }
-            }
-        });
-    });
 
     $('.check').blur(function(){
         if(!$(this).val()){
@@ -41,6 +26,47 @@ $(document).ready(function () {
                 $(this).addClass('input-error');
             } else {
                 $(this).removeClass('input-error');
+            }
+        }
+    });
+
+    emailjs.init("user_j2Jyz9OGeGOA3eezcUCiP");
+
+    $('#form-main').on('submit', function(event) {
+        event.preventDefault(); // prevent reload
+
+        var first_name = $('#first_name').val();
+        var last_name = $('#last_name').val();
+        var phone = $('#phone').val();
+        var email = $('#email').val();
+        var message = $('#message').val();
+
+        var template_params = {
+            "from_name": `${last_name} ${first_name}`,
+            "to_name": "Linh",
+            "message_html": `<p>Phone: ${phone}</p><p>Email: ${email}</p><p>Message: ${message}</p>`
+        };
+
+        var service_id = "nhokkuteo1996_gmail_com";
+        var template_id = "template_8ZRfOlaD";
+
+        var check_input = $("#form-main .check");
+
+        if (check_input.val() === '') {
+            check_input.addClass('input-error');
+        } else if (check_input.val() !== '') {
+            var email_validate = $('input[type="email"]').val();
+            if (!validateEmail(email_validate)) {
+                $('input[type="email"]').addClass('input-error');
+            } else {
+                check_input.removeClass('input-error');
+                $('.send-infomation').html('Đang gửi...').attr('disabled', 'disabled');
+                emailjs.send(service_id, template_id, template_params)
+                    .then(function(response) {
+                        showPopupSuccess();
+                    }, function(error) {
+                        alert('Gửi thông tin thất bại. Vui lòng thử lại sau.')
+                    });
             }
         }
     });
